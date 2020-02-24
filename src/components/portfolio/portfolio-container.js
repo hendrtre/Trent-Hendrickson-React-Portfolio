ffcc1e5aac02f843d8de9a66d0b5a7dfc0c305b6
Trent Hendrickson <trentdev2020@gmail.com>
2020-02-24 14:33:47 -0700

    Build a data filter

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -9,14 +9,22 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "welcome to my portfolio",
             data: [
-                {ttile: "Quip"},
-                {title: "Eventbrite"},
-                {title: "Ministry Safe"},
-                {title: "SwingAway"}
+                {ttile: "Quip", catagory: "eCommerce"},
+                {title: "Eventbrite", catagory: "Scheduling"},
+                {title: "Ministry Safe", catagory: "Enterprise"},
+                {title: "SwingAway", catagory: "eCommerce"}
             ]
         }
 
-        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this)
+        this.handleFilter = this.handleFilter.bind(this)
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.catagory === filter;
+            })
+        })
     }
 
     PortfolioItems() {
@@ -26,22 +34,18 @@ export default class PortfolioContainer extends Component {
 
     }
 
-    handlePageTitleUpdate() {
-        this.setState({
-            pageTitle: "Something ELse"
-        })
-    }
-
+    
     render() {
         return (
             <div>
                 <h2>Portfolio items go here updated...</h2>
 
+                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+
                 {this.PortfolioItems()}
 
-                <hr/>
-
-                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
             </div>
         )
     }
