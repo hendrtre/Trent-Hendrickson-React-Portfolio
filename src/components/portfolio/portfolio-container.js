96439438ca4bdc50f211da5a9376af3d90ee6949
Trent Hendrickson <trentdev2020@gmail.com>
2020-02-26 16:04:08 -0700

    Added Mixins to create button styles

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,7 +8,6 @@ export default class PortfolioContainer extends Component {
         super();
 
         this.state = {
-            pageTitle: "Welcome to my portfolio",
             isLoading: false,
             data: []
         }
@@ -60,19 +59,13 @@ export default class PortfolioContainer extends Component {
             return <div>Loading...</div>
         }
 
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
+        return (              
                 <div className="portfolio-items-wrapper">
+                    <button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                    <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                    <button className="btn" onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
                     {this.portfolioItems()}
                 </div>
-
-            </div>
         )
     }
 }
