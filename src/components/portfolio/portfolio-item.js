58fdae99b98559168e23bee6a3bb9600d964c1f8
Trent Hendrickson <trentdev2020@gmail.com>
2020-02-26 15:06:33 -0700

    Overlaying Text and Images on a Background Image

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,20 +2,23 @@ import React from "react";
 import { Link } from 'react-router-dom'
 
 export default function(props) {
-        // Data that we'll need:
-        // -Background image: thumb_image_url
-        // -logo
-        // -description: description
-        // -id: id
-        // ""
-
-        const { id, description, thumb_image_url, logo} = props.item
+        const { id, description, thumb_image_url, logo_url} = props.item
     return (
         <div className="portfolio-item-wrapper">
-            <img src={thumb_image_url} />
-            <img src={logo} />
-            <div>{description}</div>      
-            <Link to={`/portfolio/${id}`}>Link</Link>
+            <div 
+                className="portfolio-img-background"
+                style={{
+                    backgroundImage: "url(" + thumb_image_url + ")"
+                }}
+            />
+
+            <div className="img-text-wrapper">
+                <div className="logo-wraper">
+                    <img src={logo_url} />
+                </div>
+
+                <div className="subtitle">{description}</div>
+            </div>
         </div>
     )
 }
