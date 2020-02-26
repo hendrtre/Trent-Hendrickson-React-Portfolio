da6f8a6537d1aa62f49e3e942655f8bbd4911b94
Trent Hendrickson <trentdev2020@gmail.com>
2020-02-26 11:59:42 -0700

    changed some settings to automatically allow me to do autocompletes for each new file in the '.vscode'and i added a navigation link wrapper styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -13,26 +13,35 @@ export default class NavigationComponent extends Component {
     render() {
         return (
             <div className="nav-wrapper">
-            <div className="left-side">
-                <NavLink exact to="/" activeClassName="nav-link-active">
-                    Home
-                </NavLink>
+                <div className="left-side">
 
-                <NavLink to="/about-me" activeClassName="nav-link-active">
-                    About
-                </NavLink>
-                
-                <NavLink to="/contact" activeClassName="nav-link-active">
-                    Contact
-                </NavLink>
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to="/" activeClassName="nav-link-active">
+                            Home
+                        </NavLink>
+                    </div>
 
-                <NavLink to="/blog" activeClassName="nav-link-active">
-                    Blog
-                </NavLink>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/about-me" activeClassName="nav-link-active">
+                            About
+                        </NavLink>
+                    </div>
+                    
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/contact" activeClassName="nav-link-active">
+                            Contact
+                        </NavLink>
+                    </div>
+
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/blog" activeClassName="nav-link-active">
+                            Blog
+                        </NavLink>
+                    </div>
             
-                {false ? <button>Add Blog</button> : null }
-            </div>
-            <div className="right-side">Trent Hendrickson</div>
+                </div>
+                
+                <div className="right-side">Trent Hendrickson</div>
             </div>
         )
     }
