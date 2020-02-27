e794d1765f8393c4209158095e23598d19d2c3f6
Trent Hendrickson <trentdev2020@gmail.com>
2020-02-27 13:38:33 -0700

    part 1-updated some styling to the login auth page (new 'auth.scss' and 'login.js), part 2 created first form that allows imput into login info.

---
+++ src/components/auth/login.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    handleChange(event) {
+        console.log("Handle change", event)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>LOG IN TO ACCESS YOUR DASHBOARD</h1>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="Your email"
+                        value={this.state.emal}
+                        onChange={this.handleChange}
+                    />
+
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder="Your password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import loginImg from "../../../static/assets/images/auth/login.jpg"
+import Login from '../auth/login'
 
 export default class Auth extends Component {
     render() {
@@ -14,7 +15,7 @@ export default class Auth extends Component {
                 />
 
                 <div className="right-column">
-                    <h1>Login component goes here...</h1>
+                    <Login />
                 </div>
             </div>
         );
