602824593e3f83929c6e73ec249665e4f6747c3c
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-02 16:29:39 -0700

    Adding the Banner and Logo Image Uploaders

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -25,11 +25,25 @@ export default class PortfolioForm extends Component {
         this.componentConfig = this.componentConfig.bind(this)
         this.djsConfig = this.djsConfig.bind(this)
         this.handleThumbDrop = this.handleThumbDrop.bind(this)
+        this.handleBannerDrop = this.handleBannerDrop.bind(this)
+        this.handleLogoDrop = this.handleLogoDrop.bind(this)
     }
 
     handleThumbDrop() {
         return {
             addedfile: file => this.setState({ thumb_image: file })
+        }
+    }
+
+    handleBannerDrop() {
+        return {
+            addedfile: file => this.setState({ banner_image: file })
+        }
+    }
+
+    handleLogoDrop() {
+        return {
+            addedfile: file => this.setState({ logo: file })
         }
     }
 
@@ -56,10 +70,16 @@ export default class PortfolioForm extends Component {
         formData.append("portfolio_item[url]", this.state.url)
         formData.append("portfolio_item[category]", this.state.category)
         formData.append("portfolio_item[position]", this.state.position)
-        formData.append("portfolio_item[thumb_imagge]", this.state.thumb_image)
+        formData.append("portfolio_item[thumb_image]", this.state.thumb_image)
 
         if (this.state.thumb_image) {
             formData.append("portfolio_item[thumb_image]", this.state.thumb_image)
+        }
+        if (this.state.banner_image) {
+            formData.append("portfolio_item[banner_image]", this.state.banner_image)
+        }
+        if (this.state.logo) {
+            formData.append("portfolio_item[logo]", this.state.logo)
         }
 
         return formData
@@ -145,7 +165,18 @@ export default class PortfolioForm extends Component {
                             djsConfig={this.djsConfig()}
                             eventHandlers={this.handleThumbDrop()}
                         >
-
+                        </DropzoneComponent>
+                        <DropzoneComponent
+                            config={this.componentConfig()}
+                            djsConfig={this.djsConfig()}
+                            eventHandlers={this.handleBannerDrop()}
+                        >
+                        </DropzoneComponent>
+                        <DropzoneComponent
+                            config={this.componentConfig()}
+                            djsConfig={this.djsConfig()}
+                            eventHandlers={this.handleLogoDrop()}
+                        >
                         </DropzoneComponent>
                     </div>
 
