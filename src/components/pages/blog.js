1f9c6a44f87e2fd897fe6ef6799e088e059cd8c8
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-06 16:02:47 -0700

    Refactoring the Infinite Scroll Feature to Fix Memory Leak

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -16,27 +16,27 @@ export default class Blog extends Component {
         }
 
         this.getBlogItems = this.getBlogItems.bind(this)
-        this.activateInfiniteScroll()
+        this.onScroll = this.onScroll.bind(this)
+        window.addEventListener("scroll", this.onScroll, false)
     }
 
-    activateInfiniteScroll() {
-        window.onscroll = () => {
-            if (
-                this.state.isLoading || 
-                this.state.blogItems.length === this.state.totalCount
+    onScroll() {
+        if (
+            this.state.isLoading || 
+            this.state.blogItems.length === this.state.totalCount
+        ) {
+            return
+        }
+
+
+        if (
+            window.innerHeight + document.documentElement.scrollTop === 
+            document.documentElement.offsetHeight
             ) {
-                return
-            }
-
-
-            if (
-                window.innerHeight + document.documentElement.scrollTop === 
-                document.documentElement.offsetHeight
-                ) {
-                    this.getBlogItems()
-            }
+                this.getBlogItems()
         }
     }
+    
 
     getBlogItems() {
         this.setState({
@@ -58,6 +58,10 @@ export default class Blog extends Component {
 
     componentWillMount() {
         this.getBlogItems()
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.onScroll, false)
     }
 
     render() {
