c9fe6b37818af75fefeb5540188f4ed487022691
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-06 13:34:35 -0700

    Getting the Count of Total Records on the Server and Updating the Component State

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -8,7 +8,9 @@ export default class Blog extends Component {
         super()
 
         this.state = {
-            blogItems: []
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0
         }
 
         this.getBlogItems = this.getBlogItems.bind(this)
@@ -27,10 +29,15 @@ export default class Blog extends Component {
     }
 
     getBlogItems() {
+        this.setState({
+            currentPage: this.state.currentPage + 1
+        })
+
         axios.get("https://trenthendrickson.devcamp.space/portfolio/portfolio_blogs", { withCredentials: true })
         .then(response => {
             this.setState({
-                blogItems: response.data.portfolio_blogs
+                blogItems: response.data.portfolio_blogs,
+                totalCount: response.data.meta.total_records 
             })
         }).catch(error => {
             console.log("getBlogItems", error)
