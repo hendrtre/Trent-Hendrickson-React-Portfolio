1c13cbf4dc26e6b229646da7c80017b28d6f1622
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-06 10:54:22 -0700

    Rendering the Blog Details to the Screen

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -14,7 +14,10 @@ export default class BlogDetail extends Component {
     getBlogItem() {
         axios.get(`https://trenthendrickson.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
         ).then(response => {
-            console.log("response", response)
+            // console.log("response", response)
+            this.setState({
+                blogItem: response.data.portfolio_blog
+            })
         }).catch(error => {
             console.log("getBlogItem error", error)
         })
@@ -25,10 +28,20 @@ export default class BlogDetail extends Component {
     }
 
     render() {
-        console.log("currentId", this.state.currentId)
+        const {
+            title,
+            content,
+            featured_image_url,
+            blog_status
+        } = this.state.blogItem
+
         return (
             <div>
-                <h1>Blog Detail</h1>
+                <h1>{title}</h1>
+                <img src={featured_image_url} />
+                <div>
+                    {content}
+                </div>
             </div>
         )
     }
