dfbdffbc50bcf344401272e6b49dd37906b6135a
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-06 17:19:53 -0700

    How to Close the React Modal

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -5,9 +5,15 @@ export default class BlogModal extends Component {
     constructor(props) {
         super(props)
     }
+
     render() {
         return (
-            <ReactModal isOpen={this.props.modalIsOpen}>
+            <ReactModal 
+                onRequestClose={() => {
+                    this.props.handleModalClose()
+                }} 
+                isOpen={this.props.modalIsOpen}
+            >
                 <h1>I'm in a modal?</h1>
             </ReactModal>
         )
