1a8c012eb53f8d868ab693b0b1cb43c944059c95
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-09 14:29:45 -0600

    Revisiting Render Props and Passing the Logged In Status to Child Components, this shows the plus in blogs when logged in or not when logged out

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -101,11 +101,13 @@ export default class Blog extends Component {
                 modalIsOpen={this.state.blogModalIsOpen} 
             />
 
+            {this.props.loggedInStatus === "LOGGED_IN" ? (
             <div className="new-blog-link">
                 <a onClick={this.handleNewBlogClick}>
                     <FontAwesomeIcon icon="plus-circle" />
                 </a>
             </div>
+            ) : null}
 
                 <div className="content-container">{blogRecords}</div>
 
