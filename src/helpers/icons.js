b2cd163654c66b4850f0db3a867b5305faa54763
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-10 16:02:14 -0600

    Building Contact Page Styles for the React Portfolio

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -3,13 +3,25 @@ import {
     faSignOutAlt, 
     faEdit, 
     faJedi, 
-    faPlusCircle 
+    faPlusCircle,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt
 } from "@fortawesome/free-solid-svg-icons"
 import { library } from "@fortawesome/fontawesome-svg-core"
 
 
 const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, faJedi, faPlusCircle)   
+    return library.add(
+        faTrash, 
+        faSignOutAlt, 
+        faEdit, 
+        faJedi, 
+        faPlusCircle, 
+        faPhone,
+        faEnvelope,
+        faMapMarkedAlt
+    )   
 }
 
 export default Icons 
